9f4755a681d3b853d5a73840a2b74e114c640132
Peytonvt <peyton.pajak-leavy@mymail.champlain.edu>
2025-09-29 10:40:32 -0400

    progress on lab week 3

---
+++ week3/Functions & Event Logs Lab/deliverable1.ps1
@@ -0,0 +1,2 @@
+﻿# Get login and logoff records from Windows Events
+Get-EventLog System -Source Microsoft-Windows-Winlogon
---
+++ week3/Functions & Event Logs Lab/deliverable2.ps1
@@ -0,0 +1,25 @@
+﻿# Get login and logoff records from Windows Events and save to a variable
+# Get the last 14 days
+$loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+
+$loginoutsTable = @() # Empty array to fill customly
+for($i=0l $i $loginouts.Count; $i++){
+
+    # Creating event property value
+    $event = ""
+    if($loginouts[$i].EventID -eq 7001) {$event="Logon"}
+    if($loginouts[$i].EventID -eq 7002) {$event="Logoff"}
+    
+    # Creating user property value
+    $user = $loginouts[$i].ReplacementStrings[0]
+
+    # Adding each new line (in form of a custom object) to our empty array
+    $loginoutsTable += [PSCustomObject]@{
+        "Time" = $loginouts[$i].TimeGenerated
+        "Id" = $loginouts[$i].EventID
+        "Event" = $event
+        "User" = $user
+    }
+} # End of for 
+
+$loginoutsTable

99707e67b109a123b1193628c3284e3f81415fea
Peytonvt <peyton.pajak-leavy@mymail.champlain.edu>
2025-12-09 07:50:18 +0000

    fix

--- week12/access.bash
+++ week12/access.bash
@@ -2,7 +2,6 @@
 
 # Define the log file path
 log_file="/home/champuser/Desktop/SYS-320/week12/fileaccesslog.txt"
-email_log="/home/champuser/Desktop/SYS-320/week12/email_sent.txt"ffffffffffff
 
 # Get current timestamp
 timestamp=$(date "+%a %b %d %H-%M-%S %p %Z %Y")
@@ -10,4 +9,4 @@ timestamp=$(date "+%a %b %d %H-%M-%S %p %Z %Y")
 # Log the access event
 echo "File was accessed $timestamp" >> "$log_file"
 
-mail -s "Access" peyton.pajak-leavy@mymail.champlain.edu < "$log_file" 2>&1 >> "$email_log"
+mail -s "Access" peyton.pajak-leavy@mymail.champlain.edu < "$log_file"

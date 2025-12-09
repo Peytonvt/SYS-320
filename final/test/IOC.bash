2e89c529a49f652d286706879c00585fcb430d22
Peyton <122904217+Peytonvt@users.noreply.github.com>
2025-12-08 22:01:34 -0500

    Add files via upload

---
+++ final/test/IOC.bash
@@ -0,0 +1,24 @@
+#! /bin/bash
+
+# This is the link we will scrape
+link="10.0.17.6/IOC.html"
+
+# get it with curl and tell curl not to give errors
+fullPage=$(curl -sL "$link")
+
+# Utilizing xmlstarlet tool to extract table from the page
+toolOutput=$(echo "$fullPage" | \
+xmlstarlet format --html --recover 2>/dev/null | \
+xmlstarlet select --template --copy-of \
+"//html//body//table//tr")
+
+echo "$toolOutput" | \
+	sed -n 's|<td>\(.*\)</td>|\1|p'| \
+        sed 's/&#13;//g' | \
+	sed 's/^\t*//' | \
+	awk 'NR % 2 == 1' > IOC.txt
+
+
+
+
+
